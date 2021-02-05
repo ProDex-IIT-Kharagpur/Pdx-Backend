1683db448c0a19dfbbebfdd690ae79dd3ae8bff2
Godzilla5111 <singhanurag5111@gmail.com>
2021-02-06 02:44:33 +0530

    Update handlers.py

--- website/error_pages/handlers.py
+++ website/error_pages/handlers.py
@@ -5,7 +5,7 @@ error_pages = Blueprint('error_pages', __name__)
 
 @error_pages.app_errorhandler(404)
 def error_404(error):
-    return render_template('error_pages/404.html')
+    return render_template('error_pages/404.html'), 404
 
 
 @error_pages.app_errorhandler(403)
